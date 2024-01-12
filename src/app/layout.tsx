5d3396c9ed75302b6512cfd2437313fc246ab531
Dercio João <derciojdsd@gmail.com>
2024-01-12 17:43:59 +0200

    feat: setup typograph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,36 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import localFont from 'next/font/local';
+import './global.scss';
 
-const inter = Inter({ subsets: ['latin'] });
+const basisGrotesqueArabicPro = localFont({
+  src: [
+    {
+      path: '../assets/fonts/BasisGrotesqueArabicPro-Light.woff2',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/BasisGrotesqueArabicPro-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/BasisGrotesqueArabicPro-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/BasisGrotesqueArabicPro-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/BasisGrotesqueArabicPro-Black.woff2',
+      weight: '900',
+      style: 'normal',
+    },
+  ],
+});
 
 export const metadata: Metadata = {
   title: 'Serene',
@@ -15,7 +44,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={basisGrotesqueArabicPro.className}>{children}</body>
     </html>
   );
 }
