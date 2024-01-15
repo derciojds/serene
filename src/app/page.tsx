38a2ea4ebc28d7c70d348c5ee12d4b96ef3b3e5e
Dercio João <derciojdsd@gmail.com>
2024-01-16 00:41:15 +0200

    feat: implement `shopifyFetch` function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,20 @@
-import Image from 'next/image';
+import { shopifyFetch } from '@/services/shopify';
 
-export default function Home() {
+export default async function Home() {
+  const products = await shopifyFetch({
+    query: `
+      {
+        shop {
+          name
+        }
+      }
+    `,
+  });
+
+  const { body } = products;
+
+  console.log(body);
+
   return (
     <main>
       <h1>Home</h1>
---
+++ src/services/shopify/index.ts
@@ -0,0 +1,68 @@
+import { ensureStartsWith } from '@/utils';
+import { isShopifyError } from './type-guards';
+
+const domain = process.env.SHOPIFY_STORE_DOMAIN
+  ? ensureStartsWith(process.env.SHOPIFY_STORE_DOMAIN, 'https://')
+  : '';
+const endpoint = `${domain}/api/2024-01/graphql.json`;
+const key = process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN!;
+
+type ExtractVariables<T> = T extends { variables: object }
+  ? T['variables']
+  : never;
+
+export async function shopifyFetch<T>({
+  cache = 'force-cache',
+  headers,
+  query,
+  tags,
+  variables,
+}: {
+  cache?: RequestCache;
+  headers?: HeadersInit;
+  query: string;
+  tags?: string[];
+  variables?: ExtractVariables<T>;
+}): Promise<{ status: number; body: T } | never> {
+  try {
+    const result = await fetch(endpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Shopify-Storefront-Access-Token': key,
+        ...headers,
+      },
+      body: JSON.stringify({
+        ...(query && { query }),
+        ...(variables && { variables }),
+      }),
+      cache,
+      ...(tags && { next: { tags } }),
+    });
+
+    const body = await result.json();
+
+    if (body.errors) {
+      throw body.errors[0];
+    }
+
+    return {
+      status: result.status,
+      body,
+    };
+  } catch (e) {
+    if (isShopifyError(e)) {
+      throw {
+        cause: e.cause?.toString() || 'unknown',
+        status: e.status || 500,
+        message: e.message,
+        query,
+      };
+    }
+
+    throw {
+      error: e,
+      query,
+    };
+  }
+}
