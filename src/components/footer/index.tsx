93627e4fe0b327293b30ab4084bff6392ed44af7
Dercio João <derciojdsd@gmail.com>
2024-01-25 18:50:15 +0200

    feat: add scroll based animation to the bottom logo and the scrollTop button

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,86 +1,125 @@
+'use client';
+
 import { cn } from '@/utils';
+import { motion, useScroll, useSpring, useTransform } from 'framer-motion';
 import Link from 'next/link';
-import { ChevronDown, Logo } from '../Icons';
+import { useEffect, useRef, useState } from 'react';
+import { ChevronDown, Logo, ScrollTop } from '../Icons';
 import { Button } from '../button';
 import styles from './footer.module.scss';
 
 export function Footer() {
+  const footerRef = useRef(null);
+  const [scrollTop, setScrollTop] = useState(false);
+
+  const { scrollYProgress } = useScroll({
+    target: footerRef,
+    offset: ['start start', 'end 100vh'],
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], [-200, 0]);
+  const rotate = useTransform(scrollYProgress, [0, 1], [-180, 0]);
+
+  useEffect(() => {
+    if (scrollTop) {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+      return setScrollTop(false);
+    }
+  }, [scrollTop]);
+
   return (
-    <footer className={cn(styles.footerWrapper, 'theme-dark')}>
+    <footer
+      ref={footerRef}
+      className={cn(styles.footer, 'theme-dark', 'footer')}
+    >
       <div className={styles.footerSpacer}></div>
-      <div className={styles.footerContent}>
+      <div className={cn(styles.footerContent, 'container-narrow')}>
         <div className={styles.footerPadding}></div>
-        <div className="container-narrow">
-          <div className={styles.logo}>
-            <Logo />
-          </div>
-          <section className={styles.groups}>
-            <nav className={styles.navsGroup}>
-              <div>
-                <h3>About</h3>
-                <ul>
-                  <li>
-                    <Link href="#">About</Link>
-                  </li>
-                  <li>
-                    <Link href="#">Team</Link>
-                  </li>
-                  <li>
-                    <Link href="#">Blog</Link>
-                  </li>
-                </ul>
-              </div>
-
-              <div>
-                <h3>Contact</h3>
-                <ul>
-                  <li>
-                    <Link href="#">Contact</Link>
-                  </li>
-                  <li>
-                    <Link href="#">Support</Link>
-                  </li>
-                </ul>
-              </div>
-
-              <div>
-                <h3>Legal</h3>
-                <ul>
-                  <li>
-                    <Link href="#">Privacy Policy</Link>
-                  </li>
-                  <li>
-                    <Link href="#">Terms of Service</Link>
-                  </li>
-                </ul>
-              </div>
-            </nav>
-            <div className={styles.actionsGroup}>
-              <Button.root variant="secondary" size="sm" element="button">
-                <Button.content text="English" />
-                <Button.icon icon={ChevronDown} />
-              </Button.root>
+        <motion.button
+          onClick={() => setScrollTop(true)}
+          className={styles.scollTopBtn}
+          type="button"
+          style={{ rotate }}
+        >
+          <ScrollTop />
+        </motion.button>
+        <motion.picture
+          className={styles.logo}
+          style={{
+            y,
+            opacity: useSpring(scrollYProgress),
+          }}
+        >
+          <Logo />
+        </motion.picture>
+        <section className={styles.content}>
+          <nav className={styles.nav}>
+            <div>
+              <h3>About</h3>
+              <ul>
+                <li>
+                  <Link href="#">About</Link>
+                </li>
+                <li>
+                  <Link href="#">Team</Link>
+                </li>
+                <li>
+                  <Link href="#">Blog</Link>
+                </li>
+              </ul>
             </div>
-          </section>
 
-          <section className={styles.copyright}>
-            <p>© {`${new Date().getFullYear()}`} All rights reserved.</p>
-            <p>
-              Made with{' '}
-              <span role="img" aria-label="heart">
-                ❤️
-              </span>{' '}
-              by{' '}
-              <a href="#" target="_blank" rel="noreferrer">
-                Dercio JdS
-              </a>{' '}
-              •{' '}
-              <a href="#" target="_blank" rel="noreferrer">
-                Source code
-              </a>
-            </p>
-          </section>
-        </div>
+            <div>
+              <h3>Contact</h3>
+              <ul>
+                <li>
+                  <Link href="#">Contact</Link>
+                </li>
+                <li>
+                  <Link href="#">Support</Link>
+                </li>
+              </ul>
+            </div>
+
+            <div>
+              <h3>Legal</h3>
+              <ul>
+                <li>
+                  <Link href="#">Privacy Policy</Link>
+                </li>
+                <li>
+                  <Link href="#">Terms of Service</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <div>
+            <Button.root variant="secondary" size="sm" element="button">
+              <Button.content text="English" />
+              <Button.icon icon={ChevronDown} />
+            </Button.root>
+          </div>
+        </section>
+        <section className={styles.copyright}>
+          <p>© {`${new Date().getFullYear()}`} All rights reserved.</p>
+          <p>
+            Made with{' '}
+            <span role="img" aria-label="heart">
+              ❤️
+            </span>{' '}
+            by{' '}
+            <a href="#" target="_blank" rel="noreferrer">
+              Dercio JdS
+            </a>{' '}
+            •{' '}
+            <a href="#" target="_blank" rel="noreferrer">
+              Source code
+            </a>
+          </p>
+        </section>
         <div className={styles.footerPadding}></div>
       </div>
     </footer>
