4fd94f9195a20cf299be1877856f1b80c09c3f80
Dercio João <derciojdsd@gmail.com>
2024-01-29 23:51:50 +0200

    feat: create `Newsletter` section

---
+++ src/app/sections/Newsletter.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image';
+import styles from '../home.module.scss';
+
+export function Newsletter() {
+  return (
+    <section className={styles.newsletter}>
+      <Image
+        width={200}
+        height={200}
+        src="/images/newsletter.jpg"
+        alt="newsletter"
+        className={styles.image}
+      />
+      <h1 className="fs-display">
+        Get Exclusive Offers and <span className="gradient-text">Updates</span>
+      </h1>
+    </section>
+  );
+}
