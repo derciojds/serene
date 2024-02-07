8f5ccdc978e15ab1f15242b173442a3609202885
Dercio João <derciojdsd@gmail.com>
2024-02-07 21:25:14 +0200

    refactor: update variable names

--- src/lib/shopify/constants.ts
+++ src/lib/shopify/constants.ts
@@ -52,8 +52,8 @@ export const HIDDEN_PRODUCT_TAG = 'nextjs-frontend-hidden';
 export const DEFAULT_OPTION = 'Default Title';
 export const SHOPIFY_GRAPHQL_API_ENDPOINT = '/api/2024-01/graphql.json';
 
-export const domain = process.env.SHOPIFY_STORE_DOMAIN
-  ? ensureStartsWith(process.env.SHOPIFY_STORE_DOMAIN, 'https://')
+export const domain = process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN
+  ? ensureStartsWith(process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN, 'https://')
   : '';
 export const endpoint = `${domain}${SHOPIFY_GRAPHQL_API_ENDPOINT}`;
-export const key = process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN!;
+export const key = process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN!;
