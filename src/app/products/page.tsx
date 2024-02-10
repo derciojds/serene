9c2fac4f33f61dbdaf91fe908de177921bb082a2
Dercio João <derciojdsd@gmail.com>
2024-02-10 22:23:43 +0200

    feat: query products using tanstack query, implement infinite scroll and add filter

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,67 +1,100 @@
 'use client';
 
+import { Sort } from '@/components/Icons';
+import { Filter } from '@/components/filter';
 import { Product, ProductSkeleton } from '@/components/product';
 import { getProducts } from '@/lib/shopify/operations/product';
 import { Product as TProduct } from '@/lib/shopify/types';
+import { useFilterStore } from '@/stores/filterStore';
+import { buildFilterString } from '@/utils';
 import { useInfiniteQuery } from '@tanstack/react-query';
-import { useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
 import styles from './products.module.scss';
 
 export default function ProductsPage() {
   const first = 12;
 
-  const fetchProjects = async ({ pageParam }: { pageParam?: string }) => {
+  const filter = useFilterStore((state) => state.selectedContent);
+  const filterString = buildFilterString(filter) || undefined;
+
+  const fetchProducts = async ({ pageParam }: { pageParam?: string }) => {
     const { products, pageInfo } = await getProducts({
       first,
       after: pageParam,
+      query: filterString,
     });
     return { products, pageInfo };
   };
 
-  const {
-    data,
-    error,
-    fetchNextPage,
-    hasNextPage,
-    isFetching,
-    isFetchingNextPage,
-    status,
-  } = useInfiniteQuery({
-    queryKey: ['products'],
-    queryFn: fetchProjects,
+  const { data, error, fetchNextPage, hasNextPage, status } = useInfiniteQuery({
+    queryKey: ['products', filterString],
+    queryFn: fetchProducts,
     initialPageParam: undefined,
     getNextPageParam: (lastPage) => {
+      if (lastPage.products.length === 0) {
+        return undefined;
+      }
       const { hasNextPage } = lastPage.pageInfo;
-
-      const lastProductCursor =
-        lastPage.products[lastPage.products.length - 1].cursor;
-
-      return hasNextPage ? lastProductCursor : null;
+      const { cursor } = lastPage.products[lastPage.products.length - 1];
+      return hasNextPage ? cursor : null;
     },
   });
 
-  const products = data?.pages.reduce((acc: TProduct[], page) => {
-    return [...acc, ...page.products];
+  const filteredProducts = data?.pages.reduce((acc: TProduct[], page) => {
+    let products = page.products;
+
+    if (filter['availability']?.includes('Out of Stock')) {
+      products = page.products.filter(
+        (product) => product.totalInventory === 0,
+      );
+    }
+    return [...acc, ...products];
   }, []);
 
   const { ref, inView } = useInView({
     threshold: 0,
   });
 
-  useEffect(() => {
-    if (inView && hasNextPage) {
-      fetchNextPage();
-    }
-  });
+  if (inView && hasNextPage) {
+    fetchNextPage();
+  }
 
   return (
     <main className={'container'}>
-      <div className={styles.filters}></div>
+      <div className={styles.spacer}></div>
+      <div className={styles.filters}>
+        <div>
+          <Filter
+            trigger={{ label: 'Scent' }}
+            type="checkbox"
+            content={['Floral', 'Woody', 'Fruity']}
+          />
+          <Filter
+            trigger={{ label: 'Availability' }}
+            type="radio"
+            content={['In Stock', 'Out of Stock']}
+          />
+          <Filter
+            trigger={{ label: 'Price' }}
+            type="slider"
+            min={0}
+            max={100}
+          />
+        </div>
+        <Filter
+          trigger={{
+            label: 'Sort by',
+            icon: Sort,
+            className: styles.sortButton,
+          }}
+          type="radio"
+          content={['New', 'Old']}
+        />
+      </div>
       <div className={styles.products}>
         {status === 'pending'
           ? Array.from({ length: first }, (_, i) => <ProductSkeleton key={i} />)
-          : products?.map((product) => (
+          : filteredProducts?.map((product) => (
               <Product
                 key={product.handle}
                 handle={product.handle}
