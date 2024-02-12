8797bd6084e5a5c1c597dc09f6ecbda2fde7ecf6
Dercio João <derciojdsd@gmail.com>
2024-02-12 19:38:36 +0200

    feat: add respective product link

--- src/components/product/index.tsx
+++ src/components/product/index.tsx
@@ -1,22 +1,24 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from './product.module.scss';
 
 interface ProductProps {
   image: string;
   title: string;
   price: string;
+  handle: string;
 }
 
-export function Product({ image, price, title }: ProductProps) {
+export function Product({ image, price, title, handle }: ProductProps) {
   return (
     <article className={styles.container}>
-      <a href="products/#" className={styles.image}>
+      <Link href={`products/${handle}`} className={styles.image}>
         <Image width={350} height={350} src={image} alt={title} />
-      </a>
+      </Link>
       <footer>
-        <a href="products/#">
+        <Link href={`products/${handle}`}>
           <h3 className="fs-body-md truncate">{title}</h3>
-        </a>
+        </Link>
         <p className="fs-body-sm">{price}</p>
       </footer>
     </article>
