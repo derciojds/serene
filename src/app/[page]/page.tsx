2f080c760118bfcb08509704c767d7878704a384
Dercio João <derciojdsd@gmail.com>
2024-02-26 22:13:20 +0200

    feat: add `generateMetadata` function

--- src/app/[page]/page.tsx
+++ src/app/[page]/page.tsx
@@ -1,5 +1,40 @@
-export default async function Page({ params }: { params: { page: string } }) {
-  const { page } = params;
+import { getPage } from '@/lib/shopify/operations/page';
+import type { Metadata } from 'next';
+import { notFound } from 'next/navigation';
 
-  return <h1>{page} page</h1>;
+export const runtime = 'edge';
+
+export const revalidate = 43200; // 12 hours in seconds
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { page: string };
+}): Promise<Metadata> {
+  const page = await getPage(params.page);
+
+  if (!page) return notFound();
+
+  return {
+    title: page.seo?.title || page.title,
+    description: page.seo?.description || page.bodySummary,
+    openGraph: {
+      publishedTime: page.createdAt,
+      modifiedTime: page.updatedAt,
+      type: 'article',
+    },
+  };
+}
+
+export default async function Page({ params }: { params: { page: string } }) {
+  const page = await getPage(params.page);
+
+  if (!page) return notFound();
+
+  return (
+    <>
+      <h1 className="fs-h1">{page.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: page.body }} />
+    </>
+  );
 }
